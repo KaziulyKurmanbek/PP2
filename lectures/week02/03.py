1dbcc9ba2ccd0fe211eaaa9255295328bd451b93
KaziulyKurmanbek <kurmanbek_16_07@mail.ru>
2025-01-21 23:57:08 +0500

    adding lectures

---
+++ lectures/week02/03.py
@@ -0,0 +1,6 @@
+a = 3.14
+b = "3"
+c = '3'
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
---
+++ lectures/week02/04.py
@@ -0,0 +1,5 @@
+# Operators
+a = 4
+b = 2
+print('a - b = ', a-b, type(a-b))
+print('b - a = ', b-a, type(b-a))
