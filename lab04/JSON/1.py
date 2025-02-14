0101e1687a2c13e2590e4e86c44702c6de68ec51
KaziulyKurmanbek <kurmanbek_16_07@mail.ru>
2025-02-14 16:30:21 +0500

    edit

--- lab04/JSON/1.py
+++ lab04/JSON/1.py
@@ -2,13 +2,17 @@ import json
 with open("/Users/qurmanbek/Desktop/GitHub/PP2/lab04/JSON/sample-data.json", "r") as f:
     data = json.load(f)
 print("Inherit status")
-print("="*50)
-print("DN")
-print("-"*50)
+print("="*84)
+DN = "DN"
+Description = "Description"
+Speed = "Speed"
+MTU = "MTU"
+print(f"{DN:50} {Description:20} {Speed:7} {MTU:7}")
+print("-"*84)
 for item in data["imdata"]:
     attr = item["l1PhysIf"]["attributes"]
     dn = attr.get("dn")
     descr = attr.get("descr")
     speed = attr.get("speed")
     mtu = attr.get("mtu")
-    print(f"{dn:50} {descr:11} {speed:7} {mtu}")
+    print(f"{dn:50} {descr:20} {speed:7} {mtu:7}")
